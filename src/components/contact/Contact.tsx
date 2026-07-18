55494246e9576667c34ced144d77cfea10ce5225
Kelly Núñez <kellynunezhu@gmail.com>
2026-07-18 06:14:33 -0500

    Align Contact

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -3,15 +3,19 @@ import Link from "next/link";
 import Reveal from "../util/Reveal";
 import TerminalContact from "./TerminalContact";
 
+
 export const Contact = () => {
   return (
     <section className="section-wrapper" id="contact">
-      <div className="mt-10">
-        <TerminalContact />
-      </div>
-
-      <div className="w-full text-center mb-0 md:mb-10">
-        <span className="text-zinc-700 text-sm">© 2011 - 2026 Kelly Núñez. Todos los derechos reservados.</span>
+      <div className="flex flex-col items-center justify-center">
+        <Reveal>
+          <div className="mt-0 md:mt-8">
+            <TerminalContact />
+          </div>
+          <div className="w-full text-center mb-0 md:mb-8">
+            <span className="text-zinc-700 text-sm">© 2011 - 2026 Kelly Núñez. Todos los derechos reservados.</span>
+          </div>
+        </Reveal>
       </div>
     </section>
   );
